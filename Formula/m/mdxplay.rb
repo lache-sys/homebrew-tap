d1beefc260f1d50abe0713f436421cfc0c724bfd
lache-sys <program6493@outlook.com>
2026-04-14 17:39:16 +0900

    new file:   Formula/m/mdxplay.rb

---
+++ Formula/m/mdxplay.rb
@@ -0,0 +1,16 @@
+class mdxplay < formula
+  desc "MDX music data player"
+  homepage "https://web.archive.org/web/20130217181839/http://homepage3.nifty.com/studiobreeze/software/mdxplay-e.html"
+  url "https://web.archive.org/web/20130217181839/http://homepage3.nifty.com/studiobreeze/software/bin/mdxplay-20070206.tar.gz"
+  sha256 "84033159108eaabf532aa8ecb798c3332d630f8cea862e485d43638ec6ee2191"
+  license "gpl-2.0-only"
+
+  depends_on "mdxmini" => :build
+  def install
+    system "./configure", "--disable-silent-rules", *std_configure_args
+  end
+
+  test do
+    system "false"
+  end
+end
