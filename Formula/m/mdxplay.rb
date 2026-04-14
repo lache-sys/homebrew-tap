9d43db9a7228fc921cf33c0ccf63a440bf4a3f40
lache-sys <program6493@outlook.com>
2026-04-14 17:47:19 +0900

    modified:   mdxplay.rb

--- Formula/m/mdxplay.rb
+++ Formula/m/mdxplay.rb
@@ -5,9 +5,10 @@ class Mdxplay < formula
   sha256 "84033159108eaabf532aa8ecb798c3332d630f8cea862e485d43638ec6ee2191"
   license "gpl-2.0-only"
 
-  depends_on "mdxmini" => :build
   def install
     system "./configure", "--disable-silent-rules", *std_configure_args
+    system "make"
+    system "make install"
   end
 
   test do
