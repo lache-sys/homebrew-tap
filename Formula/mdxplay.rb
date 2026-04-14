a4b175673c710947cdfd223e924108831a1d9f6d
lache-sys <program6493@outlook.com>
2026-04-14 18:03:05 +0900

    modified:   mdxplay.rb

--- Formula/mdxplay.rb
+++ Formula/mdxplay.rb
@@ -4,15 +4,13 @@ class Mdxplay < Formula
   url "https://web.archive.org/web/20130217181839/http://homepage3.nifty.com/studiobreeze/software/bin/mdxplay-20070206.tar.gz"
   sha256 "84033159108eaabf532aa8ecb798c3332d630f8cea862e485d43638ec6ee2191"
   license "gpl-2.0-only"
-
+  depends_on arch: :x86_64
   depends_on "cmake" => :build
-
   def install
     system "./configure", "--disable-silent-rules", *std_configure_args
     system "make"
     system "make", "install"
   end
-
   test do
     system "false"
   end
