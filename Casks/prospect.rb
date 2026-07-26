a9b7605452ce319117ba73e677ff5ae0ac24f110
lache-sys <program6493@outlook.com>
2026-07-26 09:20:56 +0900

    Revert "modified:   Casks/prospect.rb"
    This reverts commit 11e3a43f57d427dea79b8426d7b73462b63ed47f.
    
    modified:   Casks/prospect.rb

--- Casks/prospect.rb
+++ Casks/prospect.rb
@@ -2,8 +2,7 @@ cask "prospect" do
   version "1.2.1"
   sha256 "6b66d201ed490d5eeedf1d65167cf84cdc2cab93669c97975b9a1ab294ebc9e0"
 
-  url "https://jaromvogel.com/prospect/Prospect_v#{version.sub(/./, "_")}.zip"
->>>>>>> parent of 11e3a43 (modified:   Casks/prospect.rb)
+  url "https://jaromvogel.com/prospect/Prospect_v#{version.sub(".", "_")}.zip"
   name "Prospect"
   desc "Procreate Viewer"
   homepage "https://jaromvogel.com/prospectapp"
