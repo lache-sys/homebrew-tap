a2396bf9f6f63ae3c8ecb615e012821596bb4db3
lache-sys <program6493@outlook.com>
2026-09-14 21:02:54 +0900

    new file:   Casks/font/font-4x4kana.rb deleted:    Casks/font/font-corporate-logo-rounded-bold.rb new file:   Casks/font/font-yozakura.rb

---
+++ Casks/font/font-4x4kana.rb
@@ -0,0 +1,10 @@
+cask "font-4x4kana" do
+  version "130"
+  sha256 "5a266bcffe989d6414c3d3f19eef5bcd72cff71dedc5a7bf651b7de226839556"
+
+  url "https://ftp.vector.co.jp/65/23/114/4x4minkana#{version}.zip"
+  name "4x4 Kana Font"
+  homepage "https://www.vector.co.jp/soft/dl/data/writing/se510127.html"
+
+  font "4x4minkana/4x4kanafont.ttf"
+end
---
+++ Casks/font/font-yozakura.rb
@@ -0,0 +1,11 @@
+cask "font-yozakura" do
+  version "1"
+  sha256 "4f46b8000cfccb06699a0a7a293d10a7db4f05d58c982c163109b67e11d8b2d3"
+
+  url "https://brushfonts.com/j4a5q3p61l/font_#{version}.zip"
+  name "Yozakura"
+  homepage "https://brushfonts.com/en/index.html"
+
+  font "font_1/YozakuraJP-Light.otf"
+  font "font_1/YozakuraJP-Regular.otf"
+end
