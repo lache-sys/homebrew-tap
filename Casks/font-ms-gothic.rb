8a306c25c015aae742073cb5a01fffe89d4258de
lache-sys <program6493@outlook.com>
2026-09-14 16:04:31 +0900

    new file:   Casks/font-ms-gothic.rb new file:   Casks/font-ms-mincho.rb

---
+++ Casks/font-ms-gothic.rb
@@ -0,0 +1,11 @@
+cask "font-ms-gothic" do
+  version :latest
+  sha256 :no_check
+
+  url "https://web.archive.org/web/20060319213249/http://download.microsoft.com/download/1/4/4/1442CF2D-F84D-4E26-B7F5-F992D9688417/Office2004TestDrive_JA.dmg"
+  name "MS Gothic"
+  homepage "https://web.archive.org/web/20081207120633/http://www.microsoft.com/japan/mac/download/testdrive/office2004.mspx"
+
+  font "Microsoft Office 2004/Office/Fonts/MS Gothic.ttf"
+  font "Microsoft Office 2004/Office/Fonts/MS PGothic.ttf"
+end
