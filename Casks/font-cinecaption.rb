7a2068406d29efb5553b21ef5e04d233b7d8fc7e
lache-sys <program6493@outlook.com>
2026-09-14 16:30:50 +0900

    modified:   Casks/font-cinecaption.rb

--- Casks/font-cinecaption.rb
+++ Casks/font-cinecaption.rb
@@ -6,5 +6,7 @@ cask "font-cinecaption" do
   name "Cinecaption"
   homepage "https://web.archive.org/web/20160529000353fw_/http://chiphead.jp/font/htm/cinecaption.htm"
 
+  container type: :zip
+
   font "cinecaption#{version}.ttf"
 end
