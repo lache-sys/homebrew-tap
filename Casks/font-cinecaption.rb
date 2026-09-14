6ddfa2f055642706106c7f5735db596861011180
lache-sys <program6493@outlook.com>
2026-09-14 16:36:13 +0900

    modified:   Casks/font-cinecaption.rb

--- Casks/font-cinecaption.rb
+++ Casks/font-cinecaption.rb
@@ -1,12 +1,10 @@
 cask "font-cinecaption" do
-  version "2.28"
-  sha256 "c3378620dd2073a6eceedc7f9c0e05e61c942799acaf3ca7b9936e1782a7c15a"
+  version :latest
+  sha256 :no_check
 
-  url "https://web.archive.org/web/20160529000353/http://chiphead.jp/font/cinecaption#{version}.zip"
+  url "https://web.archive.org/web/20160529000353/http://chiphead.jp/font/cinecaption2.28.zip"
   name "Cinecaption"
   homepage "https://web.archive.org/web/20160529000353fw_/http://chiphead.jp/font/htm/cinecaption.htm"
 
-  container type: :zip
-
-  font "cinecaption#{version}.ttf"
+  font "cinecaption2.28.ttf"
 end
