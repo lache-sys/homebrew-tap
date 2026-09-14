faae71f8c3b69aa6c81b37297de80c794ad47b70
lache-sys <program6493@outlook.com>
2026-09-14 16:50:31 +0900

    new file:   Casks/font-aozora-mincho.rb modified:   Casks/font-cinecaption.rb

---
+++ Casks/font-aozora-mincho.rb
@@ -0,0 +1,16 @@
+cask "aozora-mincho" do
+  version "v0.1"
+  sha256 "343b19f2b5425b2ea080932326875cbd887ab106212ddce246f184672f8d4c17"
+
+  url "https://web.archive.org/web/20200321102301/http://blueskis.wktk.so/AozoraMincho/archive/v#{version}/aozoramincho-readme-ttf.zip"
+  name "Aozora Mincho"
+  homepage "https://web.archive.org/web/20200321102301/http://blueskis.wktk.so/AozoraMincho/download.html"
+
+  font "aozoramincho-readme-ttf/AozoraMincho-bold.ttf"
+  font "aozoramincho-readme-ttf/AozoraMincho-thin.ttf"
+  font "aozoramincho-readme-ttf/AozoraMinchoBlack.ttf"
+  font "aozoramincho-readme-ttf/AozoraMinchoHeavy.ttf"
+  font "aozoramincho-readme-ttf/AozoraMinchoLight.ttf"
+  font "aozoramincho-readme-ttf/AozoraMinchoMedium.ttf"
+  font "aozoramincho-readme-ttf/AozoraMinchoRegular.ttf"
+end
--- Casks/font-cinecaption.rb
+++ Casks/font-cinecaption.rb
@@ -1,10 +1,10 @@
 cask "font-cinecaption" do
-  version :latest
-  sha256 :no_check
+  version "2.28"
+  sha256 "c3378620dd2073a6eceedc7f9c0e05e61c942799acaf3ca7b9936e1782a7c15a"
 
-  url "https://web.archive.org/web/20160529000353/http://chiphead.jp/font/cinecaption2.28.zip"
+  url "https://web.archive.org/web/20160529000353/http://chiphead.jp/font/cinecaption#{version}.zip"
   name "Cinecaption"
   homepage "https://web.archive.org/web/20160529000353fw_/http://chiphead.jp/font/htm/cinecaption.htm"
 
-  font "cinecaption2.28/cinecaption2.28.ttf"
+  font "cinecaption#{version}/cinecaption#{version}.ttf"
 end
