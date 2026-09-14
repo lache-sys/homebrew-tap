0fdfd8f479a0c93c0e9c9fa0d72e64de8e025daf
lache-sys <program6493@outlook.com>
2026-09-14 16:14:25 +0900

    new file:   Casks/font-cinecaption.rb

---
+++ Casks/font-cinecaption.rb
@@ -0,0 +1,12 @@
+cask "font-cinecaption" do
+  version "2.28"
+  sha256 "c3378620dd2073a6eceedc7f9c0e05e61c942799acaf3ca7b9936e1782a7c15a"
+
+  url "https://web.archive.org/web/20160529000353/http://chiphead.jp/font/cinecaption2.28.zip"
+  name "Cinecaption"
+  homepage "https://web.archive.org/web/20160529000353fw_/http://chiphead.jp/font/htm/cinecaption.htm"
+
+  container type: :zip
+
+  font "cinecaption2.28.ttf"
+end
