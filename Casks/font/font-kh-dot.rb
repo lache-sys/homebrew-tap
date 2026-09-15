9db496654adf3a8e9cc0fdb3b7849a2f1f14e66e
lache-sys <program6493@outlook.com>
2026-09-15 10:24:52 +0900

    new file:   Casks/font/font-kh-dot.rb

---
+++ Casks/font/font-kh-dot.rb
@@ -0,0 +1,24 @@
+cask "font-kh-dot" do
+  version "20150527"
+  sha256 "37e07933056c0d5882924cb4cfea0989532dfa0436d7b840025443e7f7e16032"
+
+  url "https://ftp.iij.ad.jp/pub/osdn.jp/users/8/8545/khdotfont-#{version}.7z"
+  name "KH Dot Font"
+  homepage "http://jikasei.me/font/kh-dotfont"
+
+  font "KH-Dot-Akihabara-16.ttf"
+  font "KH-Dot-Dougenzaka-12.ttf"
+  font "KH-Dot-Dougenzaka-16.ttf"
+  font "KH-Dot-Hatchoubori-16.ttf"
+  font "KH-Dot-Hibiya-24.ttf"
+  font "KH-Dot-Hibiya-32.ttf"
+  font "KH-Dot-Kabutochou-16.ttf"
+  font "KH-Dot-Kagurazaka-12.ttf"
+  font "KH-Dot-Kagurazaka-16.ttf"
+  font "KH-Dot-Kodenmachou-12-Ki.ttf"
+  font "KH-Dot-Kodenmachou-12-Ko.ttf"
+  font "KH-Dot-Kodenmachou-12.ttf"
+  font "KH-Dot-Kodenmachou-16-Ki.ttf"
+  font "KH-Dot-Kodenmachou-16.ttf"
+  font "KH-Dot-Ningyouchou-16.ttf"
+end
