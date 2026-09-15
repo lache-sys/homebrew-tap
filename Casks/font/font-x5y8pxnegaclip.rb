b40a95115a5f6eeae91e8fa7c8328f7263f48e1a
lache-sys <program6493@outlook.com>
2026-09-15 19:52:55 +0900

    modified:   Casks/font/font-x5y8pxnegaclip.rb

--- Casks/font/font-x5y8pxnegaclip.rb
+++ Casks/font/font-x5y8pxnegaclip.rb
@@ -1,6 +1,6 @@
 cask "font-x5y8pxnegaclip" do
   version "1.000"
-  sha256 "efd36b1f23c21540e2603a87bae96133cc6848c3f5875f9b1d983ca653d69096"
+  sha256 "efd36b1f23c21540e2603a87bae96133cc6848c3f5875f9b1d983ca653d69095"
 
   url "https://github.com/hicchicc/x5y8pxNegaClip/releases/download/v#{version}/x5y8pxNegaClip.zip"
   name "x5y8pxNegaClip"
