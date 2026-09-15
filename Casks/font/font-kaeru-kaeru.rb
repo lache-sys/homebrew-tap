ec745ac4047e7309b5083b4c96c9421365d9a5e9
lache-sys <program6493@outlook.com>
2026-09-15 10:35:20 +0900

    new file:   Casks/font/font-kaeru-kaeru.rb

---
+++ Casks/font/font-kaeru-kaeru.rb
@@ -0,0 +1,10 @@
+cask "font-kaeru-kaeru" do
+  version :latest
+  sha256 :no_check
+
+  url "https://gitlab.com/velvetyne/kaerukaeru/-/archive/master/kaerukaeru-master.zip"
+  name "Kaeru Kaeru"
+  homepage "https://velvetyne.fr/fonts/kaeru-kaeru"
+
+  font "kaerukaeru-master/fonts/kaerukaeru-Regular.otf"
+end
